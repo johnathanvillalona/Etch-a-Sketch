ca87646585d507781d657529de068cd86edf2342
JohnVilla <129530641+johnathanvillalona@users.noreply.github.com>
2026-07-09 23:35:14 -0400

    Add complete functionality and header
    Finished adding functionality so that the button properly creates a new grid
    Added a header

--- script.js
+++ script.js
@@ -8,13 +8,34 @@ function newGrid(length, width) {
     }
 }
 
-newGrid(16, 16);
+function clearGrid() {
+    //remove container
+    let container = document.querySelector(".container");
+    let body = document.querySelector("body");
+    body.removeChild(container);
 
+    //add new container
+    let newContainer = document.createElement("div");
+    newContainer.classList.add("container");
+    body.appendChild(newContainer);
+}
 
-let sections = document.querySelectorAll(".gridSquares");
-sections.forEach((section) => {
-    section.addEventListener("mouseenter", (e) => {
-        e.target.classList.add("gridSquaresHover");
+function addButtonEvent() {
+    let sections = document.querySelectorAll(".gridSquares");
+    sections.forEach((section) => {
+        section.addEventListener("mouseenter", (e) => {
+            e.target.classList.add("gridSquaresHover");
+        });
     });
-});
+}
 
+let button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+    let gridWidth = prompt("Enter grid width: (Integer)");
+    let gridHeight = prompt("Enter grid height: (Integer)");
+    clearGrid();
+    let container = document.querySelector(".container");
+    container.style.width = `${gridWidth*20}px`;
+    newGrid(gridHeight, gridWidth)
+    addButtonEvent();
+});
